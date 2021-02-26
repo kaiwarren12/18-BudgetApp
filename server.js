e23d62870f6866b67fcca61f54546925ee95752d
Kai Warren <kaiwarren12@yahoo.com>
2021-02-25 23:56:09 -0800

    added service worker to work offline

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
+const logger = require("morgan");
+const PORT = process.env.PORT || 3005;
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 // routes
